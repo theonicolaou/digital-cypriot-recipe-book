c2a66263273d252e182c57748fabf26f03153848
Theo Nicolaou <theo@theo-nicolaou.co.uk>
2020-05-25 13:44:43 +0100

    Display all JSON data in Recipes.vue template (WIP)

--- vue-cli-app-custom/src/views/Recipes.vue
+++ vue-cli-app-custom/src/views/Recipes.vue
@@ -1,9 +1,31 @@
 <template>
   <div class="recipes">
   <h1>This is the recipes View, within maincolumn, but with a different background colour just to show</h1>
+    <h2>Recipe names:</h2>
     <ul>
-      <li v-for="recipe in recipeData" v-bind:key="recipe.id">
-        {{ recipeData.recipeName }}
+      <li v-for="recipe in recipeData.recipes" v-bind:key="recipe.id">
+        {{ recipe.recipeName }} (ID: {{ recipe.id }}, URL: {{ recipe.url }})
+      </li>
+    </ul>
+
+    <h2>Ingredients:</h2>
+    <ul>
+      <li v-for="recipe in recipeData.recipes" v-bind:key="recipe.id">
+        {{ recipe.ingredients }}
+      </li>
+    </ul>
+
+        <h2>Utensils:</h2>
+    <ul>
+      <li v-for="recipe in recipeData.recipes" v-bind:key="recipe.id">
+        {{ recipe.utensils }}
+      </li>
+    </ul>
+
+        <h2>Methods:</h2>
+    <ul>
+      <li v-for="recipe in recipeData.recipes" v-bind:key="recipe.id">
+        {{ recipe.method }}
       </li>
     </ul>
   </div>
@@ -17,8 +39,7 @@
 
     data() {
      return {
-       recipeData,
-       recipeName: ''
+       recipeData
      }
    }
  }
