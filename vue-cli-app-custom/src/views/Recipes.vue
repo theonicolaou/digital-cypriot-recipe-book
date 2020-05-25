3591ac31d7b29466df7d172cf54d2984afcef872
Theo Nicolaou <theo@theo-nicolaou.co.uk>
2020-05-25 12:56:08 +0100

    Import JSON file into Recipes.vue and display data (WIP)

--- vue-cli-app-custom/src/views/Recipes.vue
+++ vue-cli-app-custom/src/views/Recipes.vue
@@ -1,17 +1,32 @@
 <template>
   <div class="recipes">
-    <h1>This is the recipes View, within maincolumn, but with a different background colour just to show</h1>
+  <h1>This is the recipes View, within maincolumn, but with a different background colour just to show</h1>
+    <ul>
+      <li v-for="recipe in recipeData" v-bind:key="recipe.id">
+        {{ recipeData.recipeName }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'Recipes'
-}
+  import recipeData from '../json/data.json'
+
+  export default {
+    name: 'Recipes',
+
+    data() {
+     return {
+       recipeData,
+       recipeName: ''
+     }
+   }
+ }
+
 </script>
 
 <style scoped lang="scss">
-	.recipes {
-		background-color: #cccccc;
-	}
+.recipes {
+  background-color: #cccccc;
+}
 </style>
